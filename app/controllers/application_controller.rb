66b272a5afccb72031eb3eec964f72cdf567ebff
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-11-23 14:46:24 +0100

    add everything for the branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_paramater_sanitizer.permit(:sign_up, keys[:first_name, :last_name])
+
+    devise_parameter_sanitize.permit(:account_update, keys: [:first_name, :last_name ])
+
+  end
+
 end
