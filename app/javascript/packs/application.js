fc5b66a0e2d9c74ca1d3a1281659af9f5a3e3cee
louise longle <louise.longle@gmail.com>
2020-11-26 16:16:23 +0100

    uncoment come lines

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -43,7 +43,7 @@ document.addEventListener('turbolinks:load', () => {
   displayCalendar();
   dynamicRating();
   buttonReviewFunction();
-  // initMapbox();
-  // initSelect2();
-  // initAutocomplete();
+  initMapbox();
+  initSelect2();
+  initAutocomplete();
 });
