ec3ca5af7e82b9f35bec4b73d16d2ea0c74bfc64
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-11-26 13:38:46 +0100

    add map and address to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :authentication_keys => [:email]
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :street, :ZIP, :city, :country, presence: true
   has_many :bookings, dependent: :destroy
   has_many :skills, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode
+  after_validation :geocode, on: [:update, :create]
+
+  def address
+    [street, city, self.ZIP, country].compact.join(', ')
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_113856) do
+ActiveRecord::Schema.define(version: 2020_11_26_095007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,12 @@ ActiveRecord::Schema.define(version: 2020_11_25_113856) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "street", default: "2, avenue des Champs-Elysées"
+    t.integer "ZIP", default: 75000
+    t.string "city", default: "Paris"
+    t.string "country", default: "France"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
